04a790fbed19a775ca34b73f5735f1b24bd7418d
oscar370 <oscar.dga@outlook.com>
2026-02-14 23:48:41 -0600

    removed lazy load, can cause freeze

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,13 +2,12 @@ import { AnimatedRoutes } from "@/components/ui/animated-routes";
 import { ModalManager } from "@/components/ui/modal-manager";
 import dayjs from "dayjs";
 import "dayjs/locale/es";
-import { lazy, useEffect } from "react";
+import { useEffect } from "react";
 import { Toaster } from "react-hot-toast";
 import { useTranslation } from "react-i18next";
 import { BrowserRouter, Route } from "react-router-dom";
+import AppRoutes from "./routes/app-routes";
 import Landing from "./routes/landing/landing";
-
-const AppRoutes = lazy(() => import("./routes/app-routes"));
 
 function App() {
   const { i18n } = useTranslation();
